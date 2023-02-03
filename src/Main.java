5279a0418886660e76139bf76436058c757c18c7
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-02-03 09:09:44 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void printSum(int a, int b){
+
+    }
+
+    public void printProduct(int a, int b){
+        
+    }
+
+}
